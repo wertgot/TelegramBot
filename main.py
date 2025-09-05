1dfa78c1cce7275a5a7afc3565472956422a9e6e
Roman Barmin <roma.barmin.06@gmail.com>
2025-09-05 11:14:59 +0300

    bot отвечает котами в течение 100 сек

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 TEXT = 'KYS'
 MAX_COUNTER = 100
+ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
+
 
 offset = -2
 counter = 0
-chat_id: int
+cat_response: requests.Response
+cat_link: str
 
-while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={-1}').json()
-
-    '''pretty_json = json.dumps(updates, indent=4, sort_keys=True, ensure_ascii=False)
-    print(pretty_json)'''
+while counter < 100:
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
