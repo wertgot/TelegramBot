3c084e28eb546b82b040244954bc64444167fb5e
Roman Barmin <roma.barmin.06@gmail.com>
2025-09-07 16:45:42 +0300

    echo bot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import requests
-import time
-import json
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 import os
 from dotenv import load_dotenv
 
@@ -10,32 +10,32 @@ load_dotenv()
 # Получаем токен из переменных окружения
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-API_URL = 'https://api.telegram.org/bot'
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
-TEXT = 'KYS'
-MAX_COUNTER = 100
-ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
 
-offset = -2
-counter = 0
-cat_response: requests.Response
-cat_link: str
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands="start"))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-while counter < 100:
-    print('attempt =', counter)
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands="help"))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
 
-    time.sleep(1)
-    counter += 1
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
